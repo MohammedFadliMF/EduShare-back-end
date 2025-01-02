4da100af80d554aa6e13db684791e051496f1932
MohammedFadliMF <fadlimohamed155@gmail.com>
2025-01-02 02:02:22 +0000

    first commit

---
+++ src/main/java/com/edushare/edushare_backend/EdushareBackendApplication.java
@@ -0,0 +1,13 @@
+package com.edushare.edushare_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdushareBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdushareBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/edushare/edushare_backend/EdushareBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edushare.edushare_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdushareBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
