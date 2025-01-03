a3e75d83b91a78b9bb38228ab05e4a4609dd1600
MohammedFadliMF <fadlimohamed155@gmail.com>
2025-01-03 00:32:09 +0000

    feat: add User entity and initialize inheritance strategy TABLE_PER_CLASS

---
+++ src/main/java/com/edushare/edushare_backend/entities/User.java
@@ -0,0 +1,26 @@
+package com.edushare.edushare_backend.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Inheritance;
+import jakarta.persistence.InheritanceType;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@NoArgsConstructor @AllArgsConstructor @Getter @Setter
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
+public abstract class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    
+    private String name;
+    private String email;
+    private String password;
+    private String role;
+}
